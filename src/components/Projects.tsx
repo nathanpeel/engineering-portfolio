793c1d2927050bb7afbe6b47d53d2118bf35bbd8
nathanielpeel <nathanjames719@gmail.com>
2024-06-27 16:42:36 -0700

    add shelph view link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -29,9 +29,9 @@ export default function Projects(): JSX.Element {
               and MongoDB
             </p>
             <div className="flex items-center gap-5">
-              {/* <Link href="/" target="_blank">
+              <Link href="https://shelph.vercel.app/" target="_blank">
                 <MdRemoveRedEye className="w-12 h-12 hover:scale-110 hover:text-lblue transition-all ease-linear" />
-              </Link> */}
+              </Link>
               <Link href="https://github.com/nathanpeel/shelph" target="_blank">
                 <FaGithub className="w-10 h-10 hover:scale-110 hover:text-lblue transition-all ease-linear" />
               </Link>
